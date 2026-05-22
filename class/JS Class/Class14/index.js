1c57965aa7e45dc3094d3fc38071a9f2970efecb
harshgolhani2006-blip <harshgolhani2006@gmail.com>
2026-05-22 12:51:35 +0530

    Shallow and deep copy

--- class/JS Class/Class14/index.js
+++ class/JS Class/Class14/index.js
@@ -85,21 +85,46 @@
 // b=20;
 // console.log(b);
 
-let user1={
-    name:"Harsh",
-    age:19,
-    address:{
-        city:"Mumbai",
-        country:"India"
-    }
-}
-let user2={...user1};
-user2.address.city="Pune";
-console.log(user1.address.city + ", " + user1.address.country); // shallow copy
-console.log(user2.address.city + ", " + user2.address.country); // shallow copy
+// let user1={
+//     name:"Harsh",
+//     age:19,
+//     address:{
+//         city:"Mumbai",
+//         country:"India"
+//     }
+// }
+// let user2={...user1};
+// user2.address.city="Pune";
+// console.log(user1.address.city + ", " + user1.address.country); // shallow copy
+// console.log(user2.address.city + ", " + user2.address.country); // shallow copy
 
-let user2= structuredClone(user1);
-user2.address.city="Pune";
-console.log(user1.address.city + ", " + user1.address.country); // deep copy
-console.log(user2.address.city + ", " + user2.address.country); // deep copy
+// let user2= structuredClone(user1);
+// user2.address.city="Pune";
+// console.log(user1.address.city + ", " + user1.address.country); // deep copy
+// console.log(user2.address.city + ", " + user2.address.country); // deep copy
 
+// let arr1=[1,2,3,4,5];
+// arr1.map
+// console.log(Array.prototype.map);
+
+
+let str =" js js js  "
+// console.log(str.length);
+// console.log(str.toLocaleUpperCase());
+// console.log(str.toLocaleLowerCase());
+// console.log(str.trim());
+// console.log(str.includes("j"));
+// console.log(str.indexOf("s"));
+// console.log(str.substring(0,2),"substring method");  // first variable starting index and second variable ending index
+// console.log(str.substr(0,4),"substr method");  //first variable starting index and second variable length of string
+// console.log(str.split(""));
+
+// let ans= str.split("").reverse().join("");  // join method is used to convert array into string and reverse method is used to reverse the array
+// console.log(ans);
+// console.log(str.replace("js","JavaScript")); // replace method is used to replace a string with another string
+// console.log(str.replaceAll("js","JavaScript")); // replaceAll method is used to replace all occurrences of a string with another string
+// console.log(str.repeat(3)); // repeat method is used to repeat a string a specified number of times
+// console.log(str.charAt(1)); // charAt method is used to get the character at a specified index
+
+console.log(str.replaceAll("j",""));
+console.log(str.replaceAll("js","jsx"));
