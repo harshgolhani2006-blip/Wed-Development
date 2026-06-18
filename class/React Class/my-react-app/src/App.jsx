c68f4e445387624568a6979ea2cc51285943bc46
harshgolhani2006-blip <harshgolhani2006@gmail.com>
2026-06-18 17:09:59 +0530

    .

--- class/React Class/my-react-app/src/App.jsx
+++ class/React Class/my-react-app/src/App.jsx
@@ -504,31 +504,32 @@
 
 // export default App
 
-import React, { useState } from "react";
-import "./App.css";
+// import React, { useState } from "react";
+// import "./App.css";
 
-const App = () => {
-  const [count, setCount] = useState(0);
+// const App = () => {
+//   const [count, setCount] = useState(0);
 
-  return (
-    <div className="container">
-      <h1>React Counter App</h1>
+//   return (
+//     <div className="container">
+//       <h1>React Counter App</h1>
 
-      <h2>{count}</h2>
+//       <h2>{count}</h2>
 
-      <button onClick={() => setCount(count + 1)}>
-        Increment
-      </button>
+//       <button onClick={() => setCount(count + 1)}>
+//         Increment
+//       </button>
 
-      <button onClick={() => setCount(count - 1)}>
-        Decrement
-      </button>
+//       <button onClick={() => setCount(count - 1)}>
+//         Decrement
+//       </button>
 
-      <button onClick={() => setCount(0)}>
-        Reset
-      </button>
-    </div>
-  );
-};
+//       <button onClick={() => setCount(0)}>
+//         Reset
+//       </button>
+//     </div>
+//   );
+// };
 
-export default App;
+// export default App;
+
