dbafe5ed943d5dd6563f348d62d4954979279f3e
harshgolhani2006-blip <harshgolhani2006@gmail.com>
2026-06-19 10:29:14 +0530

    ChatBox-Ai

--- class/React Class/my-react-app/src/App.jsx
+++ class/React Class/my-react-app/src/App.jsx
@@ -531,4 +531,17 @@
 //   );
 // };
 
+
 // export default App;
+import React from 'react'
+import ChatBox from './ChatBox'
+
+const App = () => {
+  return (
+    <div>
+        <ChatBox/>
+    </div>
+  )
+}
+
+export default App
