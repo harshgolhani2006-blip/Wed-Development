3188a7a1ffd74a043789a7b5d359070f94f47dc4
harshgolhani2006-blip <harshgolhani2006@gmail.com>
2026-07-06 21:58:39 +0530

    Hello World

--- First/JS/work.js
+++ First/JS/work.js
@@ -1,3 +1,26 @@
-<Script>
-    
-</Script>
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", () => {
+
+   const emojis = ["🎉", "🎊", "🏆", "✨", "🥳", "🎈","😊", "🥰", "😘", "😍", "😆", "🤩", "🤗", "😎", "🎆", "🎇", "🧨", "✨", "🧧", "🎀", "🎁"];
+    for(let i=0;i<80;i++){
+
+        let emoji = document.createElement("div");
+        emoji.classList.add("emoji");
+
+        emoji.innerHTML =
+            emojis[Math.floor(Math.random()*emojis.length)];
+
+        emoji.style.left =
+            Math.random()*window.innerWidth + "px";
+
+        emoji.style.animationDuration =
+            (Math.random()*3+2) + "s";
+
+        document.body.appendChild(emoji);
+
+        setTimeout(()=>{
+            emoji.remove();
+        },5000);
+    }
+});
